d0f99108802e367cdf62ac16f556048b553266a5
Петр Яненко <without1481@gmail.com>
2023-01-20 19:22:09 +0200

    initial commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Алина on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
